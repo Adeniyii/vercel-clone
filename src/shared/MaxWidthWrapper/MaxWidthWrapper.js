48c03682eb3a05ef95fd95ee664af607755de663
Adeniyii <ifedayoadeniyi@gmail.com>
2022-04-09 15:40:31 +0100

    implement max-width wrapper

---
+++ src/shared/MaxWidthWrapper/MaxWidthWrapper.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+const MaxWidthWrapper = styled.div`
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  width: var(--page-width-with-gap);
+  padding-left: var(--page-gap);
+  padding-right: var(--page-gap);
+`;
+
+export default MaxWidthWrapper;
